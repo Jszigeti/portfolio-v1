50fc912e3e922e3e2255d28d2ddbed0f30f99369
Jszigeti <j.szigeti1993@gmail.com>
2024-06-24 22:14:13 +0200

    Logo modification

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Logo() {
   return (
     <Link href="/">
-      <h1 className="text-4xl lg:text-6xl font-semibold">
+      <h1 className="text-3xl md:text-4xl xl:text-6xl font-semibold">
         &#x7B;
         <span className="text-accent">js</span>
-        works<span className="text-accent">.</span>io&#x7D;
+        project<span className="text-accent">.</span>fr&#x7D;
       </h1>
     </Link>
   );
