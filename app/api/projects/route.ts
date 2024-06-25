278871bbea7983c77cc89687a321211e436288e6
Jszigeti <j.szigeti1993@gmail.com>
2024-06-25 13:00:57 +0200

    Fixing projects/route.ts

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,10 +1,9 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   const projects = await prisma.project.findMany({
     select: {
       id: true,
