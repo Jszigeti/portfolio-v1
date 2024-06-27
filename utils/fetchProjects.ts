ac070bae8a8a956ac7c7f878e3ea23e5ed0d4642
Jszigeti <j.szigeti1993@gmail.com>
2024-06-27 14:21:11 +0200

    Fixing API endpoint

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,5 +1,7 @@
 export async function fetchProjects() {
-  const res = await fetch("http://localhost:3000/api/projects");
+  const apiEndpoint = "/api/projects";
+
+  const res = await fetch(apiEndpoint);
   if (!res.ok) {
     throw new Error("Erreur lors de la récupération des projets");
   }
