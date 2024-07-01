267f7a73f4c241d045005af1eb2388bc1d46ccb7
Jszigeti <j.szigeti1993@gmail.com>
2024-07-01 16:45:33 +0200

    Adding projects pictures and error page

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import Button from "@/components/Button";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-16 my-80">
+      <h2 className="text-5xl font-bold text-accent text-center">
+        Page non trouvée
+      </h2>
+      <Link href="/">
+        <Button content="Retour à l'accueil" />
+      </Link>
+    </div>
+  );
+}
