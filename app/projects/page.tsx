df0c48ce03a97392f8583292d0d3fd83d95e7a79
Jszigeti <j.szigeti1993@gmail.com>
2024-07-04 15:38:10 +0200

    Fixing projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -28,7 +28,7 @@ export default function Projects() {
   }, []);
 
   return (
-    <main className="container flex flex-col gap-8 py-6 lg:py-8 my-6 lg:my-8">
+    <main className="container flex flex-col gap-8 py-6 lg:py-8 my-6 lg:my-8 min-h-[71vh]">
       <h2 className="text-5xl font-bold text-accent text-center lg:text-left">
         Projets
       </h2>
